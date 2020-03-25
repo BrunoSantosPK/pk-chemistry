dad190e5dc3785ef6199327b595ad5b53d68f563
BrunoSantosPK <bruno.ajsch@yahoo.com>
2020-03-24 21:04:26 -0300

    adicionada gui basica, para primeira nocao

--- index.php
+++ index.php
@@ -34,16 +34,18 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="testes.js"></script>
     </head>
 
     <style>
         .link { cursor: pointer; }
         .link:hover { text-decoration: underline; }
         .oculta { display: none; }
+        .conteudo { height: 300px; border: 1px solid rgb(0,0,0); }
     </style>
 
     <body>
@@ -72,7 +74,18 @@
             </div>
         </div>
 
-        <div class="container mt-5 conteudo"></div>
+        <div class="container mt-5 conteudo" id="mm">
+            <div class="row">
+                <div class="col-12">
+                    <h3>Fórmula Química: Cálculo de Massa Molar</h3>
+                    <p>Exemplo: NaCl, KCl, Fe2(SO4)3, CuSO4(H2O)5 e etc.</p>
+                    <label>Infome aqui a fórmula química</label>
+                    <input type="text" id="formula"><br>
+                    <button onclick="calcMM()">Calcular</button>
+                    <p id="resMM"></p>
+                </div>
+            </div>
+        </div>
 
     </body>
 
---
+++ testes.js
@@ -0,0 +1,31 @@
+function calcMM() {
+    const formula = $("#formula").val();
+    const url = `./ch/comando.php?acao=mm&formula=${formula}`;
+    const type = "GET";
+    const data = {};
+    const callback = (res) => {
+        const div = $("#resMM");
+        div.html("");
+        div.append(`Fórmula Química Informada: ${formula}<br>`);
+        
+        if(res.sucesso) {
+            div.append(`Massa Molar (g/mol): ${res.massaMolar.toString().replace(".", ",")}`);
+        } else {
+            div.append(`Um erro aconteceu: ${res.erro}`);
+        }
+    };
+    req(url, type, data, callback);
+}
+
+function req(url, type, data, callback) {
+    $.ajax({
+        url,
+        dataType: "json",
+        type,
+        data,
+        success: data => callback(data),
+        error: erro => {
+            console.log(erro);
+        }
+    });
+}
