69a3a720539c5e24790ded292174c80b2a98b190
BrunoSantosPK <bruno.ajsch@yahoo.com>
2020-04-28 16:40:23 -0300

    adicao de exibicao de detalhes na pagina principal

--- testes.js
+++ testes.js
@@ -9,7 +9,18 @@ function calcMM() {
         div.append(`Fórmula Química Informada: ${formula}<br>`);
         
         if(res.sucesso) {
-            div.append(`Massa Molar (g/mol): ${res.massaMolar.toString().replace(".", ",")}`);
+            div.append(`Massa Molar (g/mol): ${res.massaMolar.toString().replace(".", ",")}<br>`);
+            div.append(`Foram encontrados ${res.detalhes.length} elementos.<br>`);
+
+            // Callback para normalizar uma porcentagem
+            const norm = (valor) => {
+                return (valor * 100).toFixed(2).replace(".", ",");
+            };
+
+            // Percorre os elementos
+            for(let i = 0; i < res.detalhes.length; i++) {
+                div.append(`${i + 1}) ${res.detalhes[i].nome}: ${norm(res.detalhes[i].porcentagem)} %.<br>`);
+            }
         } else {
             div.append(`Um erro ocorreu: ${res.erro}`);
         }
